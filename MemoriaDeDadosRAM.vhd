de15ba1bd4da57b69f909ee04a3a13cb2c8d6bfc
jovitor03 <123700107+jovitor03@users.noreply.github.com>
2023-03-02 21:56:41 +0000

    Add files via upload

---
+++ MemoriaDeDadosRAM.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity MemoriaDeDadosRAM is
+    Port ( WR : in  STD_LOGIC;
+           Operando1 : in  STD_LOGIC_VECTOR (7 downto 0);
+           Constante : in  STD_LOGIC_VECTOR (7 downto 0);
+           clk : in  STD_LOGIC;
+           Dados_M : out  STD_LOGIC_VECTOR (7 downto 0));
+end MemoriaDeDadosRAM;
+
+architecture Behavioral of MemoriaDeDadosRAM is
+
+begin
+	process(clk)
+		type memory is array (0 to 255) of STD_LOGIC_VECTOR(7 downto 0);
+		variable memoria : memory;
+		begin
+			if WR = '0' then
+				Dados_M <= memoria(to_integer(unsigned(Constante)));
+			else
+				if rising_edge(clk) then
+					memoria(to_integer(unsigned(Constante))) := Operando1;
+				end if;
+			end if;
+	end process;
+end Behavioral;
--- MemoriaDeInstrucoes.vhd
+++ MemoriaDeInstrucoes.vhd
@@ -14,11 +14,10 @@ architecture Behavioral of MemoriaDeInstrucoes is
 begin
 	process(Endereco)
 		type memoria is array (0 to 255) of STD_LOGIC_VECTOR(13 downto 0);
-		variable memoria_endereco : memoria;
+		variable mem : memoria;
 		begin
-		
-		
+			opcode <= mem(to_integer(unsigned(endereco)))(13 downto 9);
+			sel_r <= mem(to_integer(unsigned(endereco)))(8);
+			constante <= mem(to_integer(unsigned(endereco)))(7 downto 0);
 	end process;
-
 end Behavioral;
-
